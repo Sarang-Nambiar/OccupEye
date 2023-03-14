5cc37a2407023b0a5fd88006983a1c31874514ad
Sarang-Nambiar <101126190+Sarang-Nambiar@users.noreply.github.com>
2023-03-14 12:00:58 +0800

    adding splashscreen

--- app/src/main/java/com/example/occupeye/MainActivity.java
+++ app/src/main/java/com/example/occupeye/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, LoadingScreen.class);
-        startActivity(intent);
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoadingScreen.class); // moving to leading screen
+                startActivity(intent);
+                finish();// end running of this activity
+            }
+        }, 1000);
+
     }
 }
