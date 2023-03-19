a466fde8ac0122f34dd00b5bf4551dd37adddc9b
Sarang-Nambiar <101126190+Sarang-Nambiar@users.noreply.github.com>
2023-03-20 02:14:40 +0800

    Added bottom nav bar

--- app/src/main/java/com/example/occupeye/LoadingScreen.java
+++ app/src/main/java/com/example/occupeye/LoadingScreen.java
@@ -2,7 +2,10 @@ package com.example.occupeye;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoadingScreen extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class LoadingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        Button button = findViewById(R.id.getstartedbutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoadingScreen.this, HomeScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
